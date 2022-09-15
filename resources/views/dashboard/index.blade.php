2f4b8f80ca9e67f7e23460f42704ca7f33e43800
paulweareframework <paul.q@weareframework.co.uk>
2022-09-15 14:51:44 +0100

    fixed bug with no settings set error on install

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -6,7 +6,7 @@
         <h1 class="flex-1 font-bold">API Product Importer Dashboard</h1>
         <p>The flow of getting products into statamic is like so:</p>
     </div>
-    @if(!is_null($settings->values['api_product_importer_route']) && !empty($settings->values['api_product_importer_route']))
+    @if(isset($settings->values['api_product_importer_route']) && !is_null($settings->values['api_product_importer_route']) && !empty($settings->values['api_product_importer_route']))
         <div class="mb-5">
             <div>
                 <h3 class="font-bold">1. API</h3>
@@ -34,7 +34,7 @@
         <div>
             <div class="mb-3">
 
-                @if(is_null($settings->values['api_product_importer_route']) || empty($settings->values['api_product_importer_route']))
+                @if(isset($settings->values['api_product_importer_route']) && is_null($settings->values['api_product_importer_route']) || empty($settings->values['api_product_importer_route']))
                     <p class="font-bold">You need to fill out the importer route in settings</p>
                 @endif
 
